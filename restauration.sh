5c4941337afdcb827592c478dab26c999bd80d31
simplon-renaudV <renaud.vivancos@free.fr>
2015-10-26 16:46:33 +0100

    Ajout de la config de git

--- restauration.sh
+++ restauration.sh
@@ -5,6 +5,14 @@ tar xzvf sauvegarde.tar
 
 sudo apt-get install terminator
 sudo apt-get install git
+
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+
+git config --global user.name "simplon-renaudV"
+git config --global user.email renaud.vivancos@free.fr
+
 sudo apt-get install apache2 php5 mysql-server libapache2-mod-php5 php5-mysql
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
